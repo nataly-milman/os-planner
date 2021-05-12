d57d366798f1f16390378eab3a7505b5ae5c961e
Nataly Milman <n.pervusiak@gmail.com>
2021-05-12 13:42:24 +0300

    template

---
+++ src/Event.java
@@ -0,0 +1,14 @@
+public class Event {
+    // fields
+
+    // constructors
+
+    // methods
+
+
+    // tests here:
+    public static void main(String []args) {
+        // test creations here
+    }
+}
+
