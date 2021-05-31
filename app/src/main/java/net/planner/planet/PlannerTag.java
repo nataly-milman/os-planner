4d20f3ba2d7d28e7b8bbfeaa9235f7346ea0cc1c
Jason Elter <jason.elter@cs.huji.ac.il>
2021-05-31 23:47:17 +0300

    Added tags to Calendar and changed PlannerTag to fit needs (also small change to test so it won't break).

--- app/src/main/java/net/planner/planet/PlannerTag.java
+++ app/src/main/java/net/planner/planet/PlannerTag.java
@@ -1,20 +1,27 @@
 package net.planner.planet;
 
-import android.util.Pair;
+import com.brein.time.timeintervals.indexes.IntervalTree;
+import com.brein.time.timeintervals.indexes.IntervalTreeBuilder;
+import com.brein.time.timeintervals.intervals.IInterval;
+import com.brein.time.timeintervals.intervals.LongInterval;
 
-import java.util.Date;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public class PlannerTag {
     private String tagName;
-    private LinkedList<long[]> forbiddenTimeIntervals;
-    private LinkedList<long[]> preferredTimeIntervals;
+    private IntervalTree forbiddenTimeIntervals;
+    private IntervalTree preferredTimeIntervals;
     private int priority;
 
     public PlannerTag(String tagName) {
         this.tagName = tagName;
-        this.forbiddenTimeIntervals = new LinkedList<>();
+        this.forbiddenTimeIntervals = IntervalTreeBuilder.newBuilder()
+                .usePredefinedType(IntervalTreeBuilder.IntervalType.LONG).build();
+        this.preferredTimeIntervals = IntervalTreeBuilder.newBuilder()
+                .usePredefinedType(IntervalTreeBuilder.IntervalType.LONG).build();
     }
 
     public String getTagName() {
@@ -37,8 +44,8 @@ public class PlannerTag {
         this.priority = priority;
     }
 
-    public LinkedList<long[]> getForbiddenTimeIntervals() {
-        return forbiddenTimeIntervals;
+    public List<IInterval> getForbiddenTimeIntervals() {
+        return new ArrayList<>(forbiddenTimeIntervals);
     }
 
     public void addForbiddenTimeInterval(long from, long until) {
@@ -46,11 +53,11 @@ public class PlannerTag {
             throw new IllegalArgumentException(
                     "Illegal time interval: Event cannot end before it starts");
         }
-        this.forbiddenTimeIntervals.add(new long[]{from, until});
+        this.forbiddenTimeIntervals.add(new LongInterval(from, until));
     }
 
-    public LinkedList<long[]> getPreferredTimeIntervals() {
-        return preferredTimeIntervals;
+    public List<IInterval> getPreferredTimeIntervals() {
+        return new ArrayList<>(preferredTimeIntervals);
     }
 
     public void addPreferredTimeInterval(long from, long until) {
@@ -58,6 +65,35 @@ public class PlannerTag {
             throw new IllegalArgumentException(
                     "Illegal time interval: Event cannot end before it starts");
         }
-        this.preferredTimeIntervals.add(new long[]{from, until});
+        this.preferredTimeIntervals.add(new LongInterval(from, until));
+    }
+
+    public Collection<?> getForbiddenCollisions(long startDate, long endDate) {
+        return forbiddenTimeIntervals.find(new LongInterval(startDate, endDate));
+    }
+
+    public Collection<?> getPreferredCollisions(long startDate, long endDate) {
+        return preferredTimeIntervals.find(new LongInterval(startDate, endDate));
+    }
+
+    public boolean isIntervalForbidden(long startDate, long endDate) {
+        return getForbiddenCollisions(startDate, endDate).isEmpty();
+    }
+
+    public boolean isIntervalPreferred(long startDate, long endDate) {
+        return getPreferredCollisions(startDate, endDate).isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlannerTag that = (PlannerTag) o;
+        return tagName.equals(that.tagName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagName);
     }
 }
